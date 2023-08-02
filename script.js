44a973bfa703da3620a82cd3b02f95d31c9793f0
lazzieeCoder <rupanrrc26@gmail.com>
2023-08-02 13:09:00 +0530

    code updated

---
+++ script.js
@@ -0,0 +1,98 @@
+// Object declare using {} and print using dot notation
+// var person ={
+//     name: "Rajini",
+//     age: 45,
+//     occupation: "actor"
+// }
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.occupation);
+// Object declare using {} and print using square []
+// var person ={
+//     name: "Rajini",
+//     age: 45,
+//     occupation: "actor"
+// }
+// console.log(person["name"]);
+// console.log(person["age"]);
+// console.log(person["occupation"]);
+// console.log(person)
+// Object declare using new method
+// var person = new Object();
+// person.name = "rajini";
+// person.age = 45;
+// person.occupation = "actor";
+// console.log(person);
+//Modifying the Object using dot
+// var person = {
+//     name: "rajini",
+//     age: 45
+// }
+// console.log(person)
+// person.name= "kamal"
+// console.log(person);
+//Modifying the Object using square[]
+// var person = {
+//     name: "rajini",
+//     age: 45
+// }
+// console.log(person);
+// person["age"]=60;
+// console.log(person);
+//Adding a new property
+// var person = {
+//     name:"kamal",
+//     age:60
+// }
+// console.log(person)
+//  person.occupation= "actor"
+//  console.log(person);
+//Removing Obj property
+//  var person = {
+//     name:"kamal",
+//     age:60,
+//     city: "chennai"
+// }
+// console.log(person);
+// delete person.city
+// console.log(person);
+//JSON.parse()
+// const a = '{"name":"kamal","age":60}';
+// console.log(a);
+// var b = JSON.parse(a);
+// console.log(b);
+//JSON.stringify
+// const c = {
+//     name: "kamal",
+//     age: 60
+// }
+// console.log(c)
+// const d = JSON.stringify(c);
+// console.log(typeof(d))
+//Nested JSON object
+// var person = {
+//     "name":"kamal",
+//     "age" :60,
+//     "occupation":"actor",
+//     "address": {
+//         "city" : "chennai",
+//         "pincode" : 60001
+//     }
+// }
+// console.log(person)
+//Array Destructuring
+//Object Destructuring
+// const obj = {
+//     name: "kamal",
+//     age:60
+//  }
+// //const{key}=objectname
+// const {name,age}= obj;
+// console.log(name);
+// console.log(age);
+//nested object destuctring
+
+
+
+
+
